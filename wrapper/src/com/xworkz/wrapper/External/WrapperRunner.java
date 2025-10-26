257fe04baaf0fd14256d888911b12ff9c019d6db
aravindbv9606 <xworkzaravind20@gmail.com>
2025-10-26 16:41:36 -0400

    wrapper class

---
+++ wrapper/src/com/xworkz/wrapper/External/WrapperRunner.java
@@ -0,0 +1,20 @@
+package com.xworkz.wrapper.External;
+
+import com.xworkz.wrapper.briagad.wrapper;
+import sun.invoke.util.Wrapper;
+
+public class WrapperRunner {
+
+    public static void main(String[] args) {
+
+       wrapper rapper = new wrapper();
+
+        Integer result = new Integer (rapper.price);
+        System.out.println(result);//boxing
+
+        Integer number= rapper.price;
+        System.out.println(number);//auto boxing
+
+
+    }
+}
---
+++ wrapper/src/com/xworkz/wrapper/briagad/wrapper.java
@@ -0,0 +1,7 @@
+package com.xworkz.wrapper.briagad;
+
+public class wrapper {
+   public int price=20;
+   public  String name="aravind";
+
+}
